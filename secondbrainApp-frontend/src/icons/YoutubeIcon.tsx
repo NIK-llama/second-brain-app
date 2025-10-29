3be84088f6532e97f5e598cd909b75d400a0ced3
NIK-llama <nikshitog@gmail.com>
2025-10-30 00:37:40 +0530

    Added Sidebar Elements re

---
+++ secondbrainApp-frontend/src/icons/YoutubeIcon.tsx
@@ -0,0 +1,22 @@
+import type { JSX } from "react";
+
+export const YoutubeIcon = (): JSX.Element => {
+  return (
+    <svg
+      fill="#000000"
+      height="25px"
+      width="25px"
+      version="1.1"
+      id="Layer_1"
+      xmlns="http://www.w3.org/2000/svg"
+      xmlnsXlink="http://www.w3.org/1999/xlink"
+      viewBox="-271 311.2 256 179.8"
+      xmlSpace="preserve"
+    >
+      <path
+        d="M-59.1,311.2h-167.8c0,0-44.1,0-44.1,44.1v91.5c0,0,0,44.1,44.1,44.1h167.8c0,0,44.1,0,44.1-44.1v-91.5
+	C-15,355.3-15,311.2-59.1,311.2z M-177.1,450.3v-98.5l83.8,49.3L-177.1,450.3z"
+      />
+    </svg>
+  );
+};
